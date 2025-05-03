0ab971350958ecbb1d43dd2a9c792556817aaee2
SEANGLAY <devolutionary.wizard@gmail.com>
2025-05-03 18:05:11 +0700

    feat: implement close transaction request

---
+++ src/main/java/com/abapayway/sdk/dto/request/CloseTransactionRequest.java
@@ -0,0 +1,30 @@
+package com.abapayway.sdk.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+
+public class CloseTransactionRequest {
+    @JsonProperty("req_time")
+    @NotBlank(message = "Request time is required")
+    private String reqTime;
+
+    @JsonProperty("tran_id")
+    @NotBlank(message = "TranId is required")
+    private String tranId;
+
+    public String getReqTime() {
+        return reqTime;
+    }
+
+    public void setReqTime(String reqTime) {
+        this.reqTime = reqTime;
+    }
+
+    public String getTranId() {
+        return tranId;
+    }
+
+    public void setTranId(String tranId) {
+        this.tranId = tranId;
+    }
+}
